0f847c70c4d5433705825ad668024cbd36022227
Elizabeth Lee <dlekwjd347@gmail.com>
2020-10-15 19:01:03 -0400

    added header component

---
+++ employeedirecthw/src/components/Header/header.js
@@ -0,0 +1,13 @@
+import React from "react";
+import "./style.css";
+
+function Header () {
+    return (
+      <div className="header">
+        <h1>Employee Directory</h1>
+        <p>Use arrows or search to filter results.</p>
+      </div>
+    )
+  }
+
+export default Header;
