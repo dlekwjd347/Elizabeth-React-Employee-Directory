a2d95802869663ca6517c7ab3b46b1288136f4a2
Elizabeth Lee <dlekwjd347@gmail.com>
2020-10-15 19:38:06 -0400

    added search component with hook

--- employeedirecthw/src/App.js
+++ employeedirecthw/src/App.js
@@ -1,29 +1,30 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Wrapper from './components/Wrapper';
-import Navbar from './components/Navbar';
+// import Wrapper from './components/Wrapper';
+// import Navbar from './components/Navbar';
 import API from './utils/API';
 class App extends Component {
 state = {
   fakePeople:[{}]
 }
 
-componentDidMount(){
-  API.getUsers().then(result => {
-    this.setState({
-      fakePeople: result.data.results
-    });
-  });
-}
-render (){
-  return (
-  <Wrapper>
-    <Navbar />
-    {/* add other components here */}
-  </Wrapper>
-  );
-}
-}
+// componentDidMount(){
+//   API.getUsers().then(result => {
+//     this.setState({
+//       fakePeople: result.data.results
+//     });
+//   });
+// }
+// render (){
+//   return (
+//   <Wrapper>
+//     {/* <Navbar /> */}
+//     {/* add other components here */}
+//   </Wrapper>
+//   );
+// }
+//   )
+// }
 
 export default App;
--- employeedirecthw/src/components/Navbar/nav.js
+++ employeedirecthw/src/components/Navbar/nav.js
@@ -1,1 +1,15 @@
 import React from 'react';
+import "./style.css";
+import "../SearchSection/search";
+
+function Nav() {
+    return (
+        <nav className="navbar navbar-expand-lg navbar-light bg-light">
+
+       <div className="search-area col-4">
+        <searchName />
+    </div>
+</nav>
+    );
+}
+export default Nav;
