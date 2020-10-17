df560dc693eca62e340246b1fbfb028cb78bbaee
Elizabeth Lee <dlekwjd347@gmail.com>
2020-10-17 15:44:38 -0400

    added sort function to all columns

--- employeedirecthw/src/components/DataArea/dataArea.js
+++ employeedirecthw/src/components/DataArea/dataArea.js
@@ -1,100 +1,114 @@
 import React, {useState, useEffect} from "react";
 import TableRow from "../TableRow/tableRow";
-import Nav from "../Navbar/nav";
+import Navbar from "../Navbar/nav";
 import API from "../../utils/API";
 import "./style.css";
 import DataAreaHook from "../../utils/dataHook"
 
 const DataArea = () => {
-      const [developerState, setDeveloperState] = useState({
-        users: [],
-        order: "descend",
-        filteredUsers: [],
-        headings: [
-          { name: "Image", width: "10%", order: "descend" },
-          { name: "name", width: "10%", order: "descend" },
-          { name: "phone", width: "20%", order: "descend" },
-          { name: "email", width: "20%", order: "descend" },
-          { name: "dob", width: "10%", order: "descend" }
-        ]
-      });
-    
-      const handleSort = heading => {
-        if (developerState.order === "descend") {
-            setDeveloperState({
-                order:"ascend"
-            })
-        } else{
-            setDeveloperState({
-                order:"descend"
-            })
-        }
-    
-        const compareFnc = (a, b) => {
-          if (developerState.order === "ascend") {
-            if (a[heading] === undefined) {
-              return 1;
-            } else if (b[heading] === undefined) {
-              return -1;
-            } else if (heading === "name") {
-              return a[heading].first.localeCompare(b[heading].first);
-            } else {
-              return b[heading] - a[heading];
-            } 
-          } else {
-        if (a[heading] === undefined){
+  const [developerState, setDeveloperState] = useState({
+      users: [],
+      order: "descend",
+      filteredUsers: [],
+      headings: [
+        { name: "Image", width: "10%", order: "descend" },
+        { name: "name", width: "10%", order: "descend" },
+        { name: "phone", width: "20%", order: "descend" },
+        { name: "email", width: "20%", order: "descend" },
+        { name: "dob", width: "10%", order: "descend" }
+      ]
+    });
+  
+    const handleSort = heading => {
+      let currentOrder = developerState.headings
+        .filter(elem => elem.name === heading)
+        .map(elem => elem.order)
+        .toString();
+  
+      if (currentOrder === "descend") {
+        currentOrder = "ascend";
+      } else {
+        currentOrder = "descend";
+      }
+  
+      const compareFnc = (a, b) => {
+        if (currentOrder === "ascend") {
+          // account for missing values
+          if (a[heading] === undefined) {
             return 1;
-        } else if (b[heading] === undefined){
+          } else if (b[heading] === undefined) {
             return -1;
-        } else if (heading ==="name"){
-            return b[heading].first.localeCompare(a[heading].first);
+          }
+          // numerically
+          else if (heading === "name") {
+            return a[heading].first.localeCompare(b[heading].first);
+          } else if (heading === "dob") {
+            return a[heading].age - b[heading].age;
+          } else {
+            return a[heading].localeCompare(b[heading]);
+          }
         } else {
-return b[heading]-  a[heading];
+          if (a[heading] === undefined) {
+            return 1;
+          } else if (b[heading] === undefined) {
+            return -1;
+          }
+          else if (heading === "name") {
+            return b[heading].first.localeCompare(a[heading].first);
+          }else if (heading === "dob") {
+            return b[heading].age - a[heading].age;
+          }  else {
+            return b[heading].localeCompare(a[heading]);
+          }
         }
-    }
-    }
-        const sortedUsers = developerState.filteredUsers.sort(compareFnc);
-
+      };
+      const sortedUsers = developerState.filteredUsers.sort(compareFnc);
+      const updatedHeadings = developerState.headings.map(elem => {
+        elem.order = elem.name === heading ? currentOrder : elem.order;
+        return elem;
+      });
+  
+      setDeveloperState({
+        ...developerState,
+        filteredUsers: sortedUsers,
+        headings: updatedHeadings
+      });
+    };
+  
+    const handleSearchChange = event => {
+      const filter = event.target.value;
+      const filteredList = developerState.users.filter(item => {
+        let values = item.name.first.toLowerCase() + " " + item.name.last.toLowerCase();
+        console.log(filter, values)
+      if(values.indexOf(filter.toLowerCase()) !== -1){
+        return item
+      };
+      });
+  
+      setDeveloperState({ ...developerState, filteredUsers: filteredList });
+    };
+  
+    useEffect(() => {
+      API.getUsers().then(results => {
+        console.log(results.data.results);
         setDeveloperState({
           ...developerState,
-          filteredUsers: sortedUsers
-});
-
- };
-   
-      const handleSearchChange = event => {
-        const filter = event.target.value;
-        const filteredList = developerState.users.filter(item => {
-          let values = item.name.first.toLowerCase();
-          return values.indexOf(filter.toLowerCase()) !== -1;
+          users: results.data.results,
+          filteredUsers: results.data.results
         });
-    
-        setDeveloperState({ 
-        ...developerState, 
-        filteredUsers: filteredList });
-      };
-
-      useEffect(() => {
-        API.getUsers().then(results => {
-          setDeveloperState({
-            ...developerState,
-            users: results.data.results,
-            filteredUsers: results.data.results
-          });
-        });
-      }, []);
-    
-      return (
-        <DataAreaHook.Provider
-          value={{ developerState, handleSearchChange, handleSort }}
-        >
-          <Nav />
-          <div className="data-area">
-            {developerState.filteredUsers.length > 0 ? <TableRow /> : <div></div>
-     }
-          </div>
-        </DataAreaHook.Provider>
-      );
-    }
-    
-    export default DataArea;
+      });
+    }, []);
+  
+    return (
+      <DataAreaHook.Provider
+        value={{ developerState, handleSearchChange, handleSort }}
+      >
+        <Navbar />
+        <div className="data-area">
+          {developerState.filteredUsers.length > 0 ? <TableRow /> : <div></div>}
+        </div>
+      </DataAreaHook.Provider>
+    );
+  };
+  
+  export default DataArea;
--- employeedirecthw/src/components/Header/header.js
+++ employeedirecthw/src/components/Header/header.js
@@ -5,7 +5,7 @@ function Header () {
     return (
       <div className="header">
         <h1>Employee Directory</h1>
-        <p>Click on "name" to sort results or search to filter results.</p>
+        <p>Click on each heading to sort results or search to filter results.</p>
       </div>
     )
   }
